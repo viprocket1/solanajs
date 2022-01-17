dbb777eae92ec93bc366280a20cac81b38c91d15
DESKTOP-P2VSRRP\tanmay <mrcooldinesh1@gmail.com>
2022-01-17 10:56:10 +0530

    wallet data & NFT data

--- nft_metadata.js
+++ nft_metadata.js
@@ -1,31 +1,26 @@
-const { Connection, clusterApiUrl, PublicKey } = require("@solana/web3.js");
-const { TOKEN_PROGRAM_ID } = require("@solana/spl-token");
+const { Connection, PublicKey, clusterApiUrl } = require("@solana/web3.js");
+const { Metadata } = require("@metaplex-foundation/mpl-token-metadata");
+
+// NFT is a mint. just like SRM, USDC ..., the only different is that NFT's supply is 1
+//
+// if we want to get NFT's metadata, first we need to know what is the mint address.
+// here I take a random DAPE as an example
+// https://explorer.solana.com/address/9MwGzSyuQRqmBHqmYwE6wbP3vzRBj4WWiYxWns3rkR7A
+//
+// tokenmeta is a PDA a which derived by mint address
+// the formula is ['metadata', metadata_program_id, mint_id]
+// is it totally fine to forget it because sdk already wrapped it for us
+
+//const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
 (async () => {
-  let publicKey = new PublicKey("B3fe93AFXriKDq2Ga5rbiPw6bjPn9qqrnposbzdfSfqp");
-  let connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
-
-  let response = await connection.getParsedTokenAccountsByOwner(publicKey, {
-    programId: TOKEN_PROGRAM_ID,
-  });
-
-  let mints = await Promise.all(
-    response.value
-      .filter(
-        (accInfo) => accInfo.account.data.parsed.info.tokenAmount.uiAmount !== 0
-      )
-      .map((accInfo) =>
-        getMetadataAccount(accInfo.account.data.parsed.info.mint)
-      )
+  //nft public key
+  let mintPubkey = new PublicKey(
+    "AuHtNbxqFJWuCTt14CXLxwKhvTx4VQ3SKp712QW3s2os"
   );
+  let tokenmetaPubkey = await Metadata.getPDA(mintPubkey);
 
-  let mintPubkeys = mints.map((m) => new PublicKey(m));
-
-  let multipleAccounts = await connection.getMultipleAccountsInfo(mintPubkeys);
-
-  let nftMetadata = multipleAccounts
-    .filter((account) => account !== null)
-    .map((account) => decodeMetadata(!account.data));
-
-  console.log(nftMetadata);
+  const tokenmeta = await Metadata.load(connection, tokenmetaPubkey);
+  console.log(tokenmeta);
 })();
---
+++ wallet_data.js
@@ -0,0 +1,48 @@
+const { TOKEN_PROGRAM_ID } = require("@solana/spl-token");
+const { Connection, PublicKey, clusterApiUrl } = require("@solana/web3.js");
+const { AccountLayout, u64 } = require("@solana/spl-token");
+
+// get token accounts by owner
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+const wallet = new PublicKey("FfLczWEt8wYoubArWX6iTKJ85inoKinvovzJ4CqRRuzX");
+
+async function main() {
+  // 1. you can fetch all token account by an owner
+  let response = await connection.getTokenAccountsByOwner(
+    wallet, // owner here
+    {
+      programId: TOKEN_PROGRAM_ID,
+    }
+  );
+  response.value.forEach((e) => {
+    console.log(`pubkey: ${e.pubkey.toBase58()}`);
+    const accountInfo = AccountLayout.decode(e.account.data);
+    console.log(`mint: ${new PublicKey(accountInfo.mint)}`);
+    console.log(`amount: ${u64.fromBuffer(accountInfo.amount)}`);
+  });
+
+  console.log("-------------------");
+
+  // 2. or just fetch specific mint for a owner
+  let response2 = await connection.getTokenAccountsByOwner(
+    wallet, // owner here
+    {
+      mint: new PublicKey("AuHtNbxqFJWuCTt14CXLxwKhvTx4VQ3SKp712QW3s2os"),
+    }
+  );
+  response2.value.forEach((e) => {
+    console.log(`pubkey: ${e.pubkey.toBase58()}`);
+    const accountInfo = AccountLayout.decode(e.account.data);
+    console.log(`mint: ${new PublicKey(accountInfo.mint)}`);
+    console.log(`amount: ${u64.fromBuffer(accountInfo.amount)}`);
+  });
+}
+
+main().then(
+  () => process.exit(),
+  (err) => {
+    console.error(err);
+    process.exit(-1);
+  }
+);
